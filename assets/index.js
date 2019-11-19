21317e59104ec1faab2a9c70e67b5314b593cac9
marnanakou <marnanakou@gmail.com>
2019-11-19 18:57:33 +0100

    generating the fields

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,27 @@
-//creating the password generator
+//creating the container
+let container = document.createElement("div");
+container.classList.add("container");
+container.style.width = "75px";
+container.style.align = "center";
+
+//Creating the password generator
+
+//using getRandomInt() function
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
 function passwordGenerator(num, mixedCase) {
   const charset = "abcdefghijklmnopqrstuvwxyz0123456789!§$%&/()=?#,;.:-_";
+  let pass = "";
+  for (let i = 0; i < num; i++) {
+    var randomIndex = getRandomInt(charset.length);
+    if (i % 3 === 0 && mixedCase === true) {
+      pass += charset[randomIndex].toUpperCase();
+    } else {
+      pass += charset[randomIndex];
+    }
+  }
+  return pass;
 }
+console.log(passwordGenerator(10, true));
